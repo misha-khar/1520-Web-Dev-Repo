5dae0bd5d1d88884516f97b4f588fdec69c12ae1
Misha <michael.kharkovski@gmail.com>
2023-03-30 11:08:46 -0400

    final touches?

--- Kharkovski_flask_models_lab/BlogModel.py
+++ Kharkovski_flask_models_lab/BlogModel.py
@@ -6,5 +6,5 @@ db = SQLAlchemy()
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    body = db.Column(db.String(500), nullable=False)
+    body = db.Column(db.String(1000), nullable=False)
     author = db.Column(db.String(50), nullable=False)
