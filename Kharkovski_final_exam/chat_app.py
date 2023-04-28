20d07154f7e307f3dd27c4000437dd62d021e2a6
Misha <michael.kharkovski@gmail.com>
2023-04-28 09:40:03 -0400

    added account creation and login

--- Kharkovski_final_exam/chat_app.py
+++ Kharkovski_final_exam/chat_app.py
@@ -1,12 +1,34 @@
 from flask import Flask, request, abort, url_for, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_BINDS'] = {'chat': 'sqlite:///chat.db'}
 
+db = SQLAlchemy()
+db.init_app(app)
 
-# class Chat(db.Model):
-#     __bind_key__ = 'chat'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), unique=True, nullable=False)
+    password = db.Column(db.String(10), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+
+
+class Chat(db.Model):
+    __bind_key__ = 'chat'
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(255), nullable=False)
+    user = db.Column(db.String(10), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+
+
+@app.route("/create_db")
+def create_db():
+    db.create_all()
+    return "all tables have been created!"
 
 
 # by default, direct to login
@@ -17,19 +39,55 @@ def default():
 
 @app.route("/login/", methods=["GET", "POST"])
 def login_controller():
-
     if request.method == "POST":
-        return render_template("chat_page.html")
+        # check if username and pass word match form db
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(
+            username=username, password=password).first()
+
+        if user is not None:
+            return redirect(url_for("profile", username=username))
+        else:
+            return render_template("loginPage.html", error="Incorrect username or password", refToRegister=url_for("register_controller"))
 
     elif request.method == "GET":
-        return render_template("loginPage.html", refToRegister=url_for("register"))
+        return render_template("loginPage.html", refToRegister=url_for("register_controller"))
 
     return render_template("loginPage.html", refToRegister=url_for("register_controller"))
 
 
 @app.route("/register/", methods=["GET", "POST"])
 def register_controller():
-    return render_template("registerPage.html")
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        # username = 'misha'
+        # email = 'test@gmail.com'
+        # password = 'abc'
+        # confirm_password = 'abc'
+        if password != confirm_password:
+            # Passwords don't match - display error message
+            error = "Passwords don't match. Please try again."
+            return render_template('register.html', error=error)
+        else:
+            try:
+                # Create new user in database and redirect to login page
+                new_user = User(username=username,
+                                email=email, password=password)
+                db.session.add(new_user)
+                db.session.commit()
+                return redirect(url_for('profile', username=username, refToLogout=url_for("unlogger")))
+            except:
+                # Display error message if username already exists
+                error = "Error creating account. Please try again."
+                return render_template('register.html', error=error)
+    else:
+        # Display registration form
+        return render_template('register.html')
 
 
 @app.route("/profile/<username>")
