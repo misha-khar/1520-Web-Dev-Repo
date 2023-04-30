39b630a0ec795812b79f9fe04a90fa0713fddfeb
Misha <michael.kharkovski@gmail.com>
2023-04-30 09:57:41 -0400

    minor edits

--- Kharkovski_final_exam/chat_app.py
+++ Kharkovski_final_exam/chat_app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request, abort, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -11,14 +10,14 @@ db = SQLAlchemy()
 db.init_app(app)
 
 
-class User(db.Model):
+class User(db.Model):  # create a User class for the app.db database
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(10), unique=True, nullable=False)
     password = db.Column(db.String(10), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
 
 
-class Chat(db.Model):
+class Chat(db.Model):  # create a Chat class for the chat.db database
     __bind_key__ = 'chat'
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(255), nullable=False)
@@ -26,19 +25,18 @@ class Chat(db.Model):
     timestamp = db.Column(db.DateTime, nullable=False)
 
 
-@app.route("/create_db")
+@app.route("/create_db")  # create the database
 def create_db():
     db.create_all()
     return "all tables have been created!"
 
 
-# by default, direct to login
-@app.route("/")
+@app.route("/")  # by default, direct to login
 def default():
     return redirect(url_for("login_controller"))
 
 
-@app.route("/login/", methods=["GET", "POST"])
+@app.route("/login/", methods=["GET", "POST"])  # login controller
 def login_controller():
     if request.method == "POST":
         # check if username and pass word match form db
@@ -59,13 +57,14 @@ def login_controller():
     return render_template("loginPage.html", refToRegister=url_for("register_controller"), errorMessage="")
 
 
-@app.route("/register/", methods=["GET", "POST"])
+@app.route("/register/", methods=["GET", "POST"])  # register controller
 def register_controller():
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
+        # confirm if passwords match
         if password != confirm_password:
             # Passwords don't match - display error message
             error = "Passwords don't match. Please try again."
@@ -75,36 +74,39 @@ def register_controller():
                 # Create new user in database and redirect to login page
                 new_user = User(username=username,
                                 email=email, password=password)
+                # add new user to the database
                 db.session.add(new_user)
+                # commit changes to the database
                 db.session.commit()
                 return redirect(url_for('profile', username=username, refToLogout=url_for("unlogger")))
             except:
                 # Display error message if username already exists
                 error = "Error creating account. Please try again."
-                return render_template('register.html', error=error)
+                return render_template('register.html', error=error, errorMessage="Username or email already exists")
     else:
-        # Display registration form
         return render_template('register.html')
 
 
-@app.route("/profile/<username>")
+@app.route("/profile/<username>")  # profile controller
 def profile(username=None):
     return render_template("chat_page.html", username=username, refToLogout=url_for("unlogger"))
 
 
-@app.route("/logout/")
+@app.route("/logout/")  # logout controller
 def unlogger():
     return render_template("logoutPage.html", refToLogin=url_for("login_controller"))
 
 
-@app.route("/new_message/", methods=["POST"])
+@app.route("/new_message/", methods=["POST"])  # new message controller
 def new_message():
     try:
         message = request.form["message"]
         user = request.form["username"]
         timestamp = datetime.now()
         new_message = Chat(message=message, user=user, timestamp=timestamp)
+        # add new message to the database
         db.session.add(new_message)
+        # commit changes to the database
         db.session.commit()
         return "success"
     except Exception as e:
@@ -112,16 +114,20 @@ def new_message():
         return "error"
 
 
-@app.route("/messages/")
+@app.route("/messages/")  # messages controller
 def messages():
     try:
+        # get all messages from the database
         rawMessages = Chat.query.order_by(Chat.timestamp.desc()).all()
         results = []
+        # create a dictionary for each message
         for message in rawMessages:
             res = {
                 message.user: message.message,
             }
+            # append each message to the results list
             results.append(res)
+            # return the results list as a json object
         return jsonify(results)
     except Exception as e:
         print(e)
--- Kharkovski_final_exam/static/js/chat_page.js
+++ Kharkovski_final_exam/static/js/chat_page.js
@@ -1,28 +1,38 @@
+// add window event listener to run setup function when page loads
 window.addEventListener('load', setup);
-let timeout = 1000;
+// let timeout = 1000;
+let timeout = 15000;
+// pretty slow timeout, could make faster
 
+// init setup func
 function setup() {
     var sendMessageButton = document.getElementById("send");
+    // grab initial data if exists from previous session
     fetchNewData();
-
+    // event listneer for send button
     sendMessageButton.addEventListener("click", function (event) {
+        // prevent default behavior
         event.preventDefault();
         var message = document.getElementById("message").value;
         var author = document.getElementById("author").innerHTML;
-
+        // if empty message, alert user
         if (message == "") {
             alert("Please enter a message");
         }
+        // else send message to server
         else {
+            // send a post request to server with message and author
             fetch("/new_message/", {
                 method: "post",
                 headers: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" },
                 body: `username=${author}&message=${message}`
             })
+                // if successful, prepend message to chat window, most recent on top
                 .then((response) => {
                     chat_window.value = `User: ${author}:\n${message}\n\n` + chat_window.value;
                     return response;
                 })
+                // if error, alert user
                 .catch(() => {
                     chat_window.value = "error sending message to server";
                 });
@@ -30,15 +40,20 @@ function setup() {
     });
 }
 
+// function to fetch new data from server
 function fetchNewData() {
     console.log("fetching new data");
+    // fetch data from server
     fetch("/messages/")
+        // if successful, update chat window with new data
         .then((response) => {
             return response.json();
         })
+        // if successful, update chat window with new data
         .then((results) => {
             let chat_window = document.getElementById("chat_window");
             let messages = "";
+            // loop through results and add to chat window
             for (let index in results) {
                 current_set = results[index];
                 for (let key in current_set) {
@@ -49,8 +64,10 @@ function fetchNewData() {
             }
             chat_window.value = messages;
         })
+        // if error, alert user
         .catch(() => {
             chat_window.value = "error retrieving messages from server";
         });
+    // set timeout to fetch new data again
     window.setTimeout(fetchNewData, timeout);
 }
